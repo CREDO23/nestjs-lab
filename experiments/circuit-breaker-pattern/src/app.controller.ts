6e679af6b0572689b2cc93dbd9adca3019f7c035
CREDO23 <thierrybakera12@gmail.com>
2025-10-15 23:08:39 +0200

    circuit-breaker-patter: prepare a controller for testing

--- experiments/circuit-breaker-pattern/src/app.controller.ts
+++ experiments/circuit-breaker-pattern/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { CircuitBreakerInterceptor } from './interceptors/circuit-breaker.interceptor';
 
+@UseInterceptors(CircuitBreakerInterceptor)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    console.log("Couldn't getHello");
+    throw new HttpException("Couldn't getHello", HttpStatus.BAD_REQUEST);
   }
 }

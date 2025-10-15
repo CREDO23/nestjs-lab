de9c2a6f0d581e5d2c54200082f4dbb737c29cc3
CREDO23 <thierrybakera12@gmail.com>
2025-10-16 01:56:51 +0200

    worker-threads: add a fibonacci endpoint

--- experiments/worker-threads/src/app.module.ts
+++ experiments/worker-threads/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggingModule } from '@experiments/shared';
+import { FibonacciController } from './fibonacci.controller';
 
 @Module({
   imports: [LoggingModule],
-  controllers: [AppController],
+  controllers: [AppController, FibonacciController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ experiments/worker-threads/src/fibonacci.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Query } from '@nestjs/common';
+
+@Controller('fibonacci')
+export class FibonacciController {
+  @Get()
+  fibonacci(@Query('n') n: number = 10): number {
+    if (n < 2) {
+      return n;
+    }
+
+    return this.fibonacci(n - 1) + this.fibonacci(n - 2);
+  }
+}

7bee53657755b8c435d4f437f626a1d418a242e4
CREDO23 <thierrybakera12@gmail.com>
2025-10-15 22:50:21 +0200

    circuit-breaker-patter: implement the circuit breaker interceptor

---
+++ experiments/circuit-breaker-pattern/src/interceptors/circuit-breaker..ts
@@ -0,0 +1,63 @@
+import { CallHandler } from '@nestjs/common';
+import { tap, throwError } from 'rxjs';
+
+const SUCCESS_THRESHOLD = 3; // the number of successful requests needed to close the circuit
+const FAILURE_THRESHOLD = 3; // the number of failed requests needed to open the circuit
+const OPEN_TO_HALF_OPEN_TIMEOUT = 60000; // the timeout in milliseconds needed to move from open to half-open state
+
+enum CIRCUIT_BREAKER_STATE {
+  CLOSED = 'closed',
+  OPEN = 'open',
+  HALF_OPEN = 'half-open',
+}
+
+export class CircuitBreaker {
+  private state: CIRCUIT_BREAKER_STATE = CIRCUIT_BREAKER_STATE.CLOSED;
+  private successCount = 0;
+  private failureCount = 0;
+  private lastError?: Error;
+  private nextAttempt: number;
+
+  exec(next: CallHandler) {
+    if (this.state === CIRCUIT_BREAKER_STATE.OPEN) {
+      if (this.nextAttempt > Date.now()) {
+        return throwError(() => this.lastError);
+      }
+
+      this.state = CIRCUIT_BREAKER_STATE.HALF_OPEN;
+    }
+
+    return next.handle().pipe(
+      tap({
+        next: () => this.handleSuccess(),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        error: (err) => this.handleFailure(err),
+      }),
+    );
+  }
+
+  private handleSuccess() {
+    this.failureCount = 0;
+
+    if (this.state === CIRCUIT_BREAKER_STATE.HALF_OPEN) {
+      this.successCount++;
+      if (this.successCount >= SUCCESS_THRESHOLD) {
+        this.state = CIRCUIT_BREAKER_STATE.CLOSED;
+        this.successCount = 0;
+      }
+    }
+  }
+
+  private handleFailure(err: Error) {
+    this.failureCount++;
+
+    if (
+      this.failureCount >= FAILURE_THRESHOLD ||
+      this.state === CIRCUIT_BREAKER_STATE.HALF_OPEN
+    ) {
+      this.state = CIRCUIT_BREAKER_STATE.OPEN;
+      this.lastError = err;
+      this.nextAttempt = Date.now() + OPEN_TO_HALF_OPEN_TIMEOUT;
+    }
+  }
+}
--- experiments/circuit-breaker-pattern/src/interceptors/circuit-breaker.interceptor.ts
+++ experiments/circuit-breaker-pattern/src/interceptors/circuit-breaker.interceptor.ts
@@ -1,63 +1,34 @@
-import { CallHandler } from '@nestjs/common';
-import { tap, throwError } from 'rxjs';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { CircuitBreaker } from './circuit-breaker.';
 
-const SUCCESS_THRESHOLD = 3; // the number of successful requests needed to close the circuit
-const FAILURE_THRESHOLD = 3; // the number of failed requests needed to open the circuit
-const OPEN_TO_HALF_OPEN_TIMEOUT = 60000; // the timeout in milliseconds needed to move from open to half-open state
+@Injectable()
+export class CircuitBreakerInterceptor implements NestInterceptor {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  private circuitBreakersRegistry = new WeakMap<Function, CircuitBreaker>();
 
-enum CIRCUIT_BREAKER_STATE {
-  CLOSED = 'closed',
-  OPEN = 'open',
-  HALF_OPEN = 'half-open',
-}
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> {
+    const targetHandler = context.getHandler();
 
-export class CircuitBreaker {
-  private state: CIRCUIT_BREAKER_STATE = CIRCUIT_BREAKER_STATE.CLOSED;
-  private successCount = 0;
-  private failureCount = 0;
-  private lastError?: Error;
-  private nextAttempt: number;
+    let circuitBreaker: CircuitBreaker;
 
-  exec(next: CallHandler) {
-    if (this.state === CIRCUIT_BREAKER_STATE.OPEN) {
-      if (this.nextAttempt > Date.now()) {
-        return throwError(() => this.lastError);
-      }
-
-      this.state = CIRCUIT_BREAKER_STATE.HALF_OPEN;
+    if (this.circuitBreakersRegistry.has(targetHandler)) {
+      circuitBreaker = this.circuitBreakersRegistry.get(
+        targetHandler,
+      ) as CircuitBreaker;
+    } else {
+      circuitBreaker = new CircuitBreaker();
+      this.circuitBreakersRegistry.set(targetHandler, circuitBreaker);
     }
 
-    return next.handle().pipe(
-      tap({
-        next: () => this.handleSuccess(),
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        error: (err) => this.handleFailure(err),
-      }),
-    );
-  }
-
-  private handleSuccess() {
-    this.failureCount = 0;
-
-    if (this.state === CIRCUIT_BREAKER_STATE.HALF_OPEN) {
-      this.successCount++;
-      if (this.successCount >= SUCCESS_THRESHOLD) {
-        this.state = CIRCUIT_BREAKER_STATE.CLOSED;
-        this.successCount = 0;
-      }
-    }
-  }
-
-  private handleFailure(err: Error) {
-    this.failureCount++;
-
-    if (
-      this.failureCount >= FAILURE_THRESHOLD ||
-      this.state === CIRCUIT_BREAKER_STATE.HALF_OPEN
-    ) {
-      this.state = CIRCUIT_BREAKER_STATE.OPEN;
-      this.lastError = err;
-      this.nextAttempt = Date.now() + OPEN_TO_HALF_OPEN_TIMEOUT;
-    }
+    return circuitBreaker.exec(next);
   }
 }

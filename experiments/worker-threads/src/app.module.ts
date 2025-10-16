3fa6754d5f3e40a785595780e64a0c43203e9fe8
CREDO23 <thierrybakera12@gmail.com>
2025-10-16 22:18:44 +0200

    worker-threads: add fibonacci module

--- experiments/worker-threads/src/app.module.ts
+++ experiments/worker-threads/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggingModule } from '@experiments/shared';
-import { FibonacciController } from './fibonacci.controller';
+import { FibonacciController } from './fibonacci/fibonacci.controller';
+import { FibonacciModule } from './fibonacci/fibonacci.module';
 
 @Module({
-  imports: [LoggingModule],
+  imports: [LoggingModule, FibonacciModule],
   controllers: [AppController, FibonacciController],
   providers: [AppService],
 })

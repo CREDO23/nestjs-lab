f7b7fa36c4dc040d3cddadfe608a2fe255267f0a
CREDO23 <thierrybakera12@gmail.com>
2025-10-20 22:35:39 +0200

    lazy-loading-modules: add a lazy loaded module

--- experiments/lazy-loading-modules/src/app.controller.ts
+++ experiments/lazy-loading-modules/src/app.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LazyModuleLoader } from '@nestjs/core';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private lazyModuleLoader: LazyModuleLoader,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('reports')
+  async getReports(): Promise<string> {
+    const reportModuleRef = await this.lazyModuleLoader.load(() =>
+      import('./reports/reports.module').then((mod) => mod.ReportsModule),
+    );
+
+    const { ReportsService } = await import('./reports/reports.service');
+
+    const reportService = reportModuleRef.get(ReportsService);
+    return reportService.getReports();
   }
 }
---
+++ experiments/lazy-loading-modules/src/reports/reports.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ReportsService {
+    getReports() {
+        return 'This action returns all reports';
+    }s
+}
